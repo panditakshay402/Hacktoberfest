c706ae3891c75e1284214421782c6e03565368a3
Devesh Chandel <devchandel15@gmail.com>
2023-10-07 03:44:52 +0530

    Maximum product Subarray

--- JAVA/DYNAMIC_PROGRAMMING/ UniquePaths.java
+++ JAVA/DYNAMIC_PROGRAMMING/ UniquePaths.java
@@ -20,7 +20,7 @@ class Solution {
         }
         for (int i = 1; i < m ; i ++){
             for (int j = 1; j < n; j ++){
-                paths[i][j] = paths[i-1][j] + paths[i][j-1];
+                paths[i][j] = paths[i-1][j] +  paths[i][j-1];
             }
         }
         return paths[m-1][n-1];
