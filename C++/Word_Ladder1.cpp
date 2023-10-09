e019ceb8b10788c03914eef6ed4bc53a509bb61d
Robinrai2612 <robinsrai2000@gmail.com>
2023-10-09 22:38:54 +0530

    Add word ladder length algorithm

--- C++/Word_Ladder1.cpp
+++ C++/Word_Ladder1.cpp
@@ -9,8 +9,15 @@ public:
     int wordLadderLength(string startWord, string targetWord,
                          vector<string> &wordList)
     {
+    // Creating a queue ds of type {word,transitions to reach ‘word’}.
         queue<pair<string, int>> q;
+
+        // BFS traversal with pushing values in queue 
+        // when after a transformation, a word is found in wordList.
         q.push({startWord, 1});
+
+        // Push all values of wordList into a set
+        // to make deletion from it easier and in less time complexity.
         unordered_set<string> st(wordList.begin(), wordList.end());
         st.erase(startWord);
         while (!q.empty())
@@ -19,15 +26,20 @@ public:
             int steps = q.front().second;
             q.pop();
 
+            // we return the steps as soon as
+            // the first occurence of targetWord is found.
             if (word == targetWord)
                 return steps;
 
             for (int i = 0; i < word.size(); i++)
             {
+                // Now, replace each character of ‘word’ with char
+                // from a-z then check if ‘word’ exists in wordList.
                 char original = word[i];
                 for (char ch = 'a'; ch <= 'z'; ch++)
                 {
                     word[i] = ch;
+                    // check if it exists in the set and push it in the queue.
                     if (st.find(word) != st.end())
                     {
                         st.erase(word);
@@ -37,6 +49,7 @@ public:
                 word[i] = original;
             }
         }
+        // If there is no transformation sequence possible
         return 0;
     }
 };
@@ -55,3 +68,4 @@ int main()
     cout << endl;
     return 0;
 }
+Output: 
