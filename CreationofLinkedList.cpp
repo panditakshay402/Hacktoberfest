02d713a3ffed189d0ab22e820bcda7a77503374c
albert_5 <130452338+nehal-5@users.noreply.github.com>
2023-10-19 19:05:29 +0530

    Create CreationofLinkedList.cpp

---
+++ CreationofLinkedList.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+struct node{
+  int data;
+  struct node *next;
+}
+
+struct node *head = NULL;
+struct node *p,*temp;
+
+void creation(int x){
+    p = (struct node*)malloc(sizeof(struct node));
+    p->data = x;
+    p->next = NULL;
+    if(head == NULL){
+        head = p;
+    }
+    else{
+        temp = head;
+        while(temp->next != NULL){
+          temp = temp->next;
+        }
+        temp->next = p;
+    }
+}
+
+void display(){
+    temp = head;
+    while(temp->next != NULL){
+          cout << temp->data << " ";
+          temp = temp->next;
+    }
+    cout << temp->data << endl;
+}
