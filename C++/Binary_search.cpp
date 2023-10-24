404bf392a83e60de0d2ee543827cc2217c638921
ls345 <146213748+ls345@users.noreply.github.com>
2023-10-23 21:24:51 -0700

    Update Binary_search.cpp
    Added code for another option for binary search for c++

--- C++/Binary_search.cpp
+++ C++/Binary_search.cpp
@@ -30,3 +30,28 @@ int main() {
 
     return 0;
 }
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// Driver's code
+int main()
+{
+    // initializing vector of integers
+    vector<int> arr = { 10, 15, 20, 25, 30, 35 };
+ 
+    // using binary_search to check if 15 exists
+    if (binary_search(arr.begin(), arr.end(), 15))
+        cout << "15 exists in vector";
+    else
+        cout << "15 does not exist";
+ 
+    cout << endl;
+ 
+    // using binary_search to check if 23 exists
+    if (binary_search(arr.begin(), arr.end(), 23))
+        cout << "23 exists in vector";
+    else
+        cout << "23 does not exist";
+ 
+    cout << endl;
+}
